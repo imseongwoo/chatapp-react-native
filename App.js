e38226f23dee3582b46c6eb5d6135833fb07a2e8
임성우 <seongwoorang@naver.com>
2021-02-15 16:05:47 +0900

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> react natidfve!</Text>
+      <Text> how to use this?!!</Text>
       <StatusBar style="auto" />
     </View>
   );
