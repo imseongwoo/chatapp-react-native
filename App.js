1018c4babf0efa17d0a188a020bf25d50d240eee
임성우 <seongwoorang@naver.com>
2021-02-15 15:55:39 +0900

    test2

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> practice chat app!</Text>
+      <Text> react natidfve!</Text>
       <StatusBar style="auto" />
     </View>
   );
