dda62e7cb250cdac96f032618ff05f2ea7483203
임성우 <seongwoorang@naver.com>
2021-03-15 22:22:54 +0900

    Update OnboardingScreen.js

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,11 +1,37 @@
 import React from 'react'
-import { View, Text, Button,Image, StyleSheet } from 'react-native'
+import { View, Text, Button,Image, StyleSheet,TouchableOpacity } from 'react-native'
 import Onboarding from 'react-native-onboarding-swiper';
 
+const Skip = ({...props}) => (
+    <Button
+        title = 'Skip'
+        color="#000000"
+        {...props}
+    />
+);
+const Next = ({...props}) => (
+    <Button
+        title = 'Next'
+        color="#000000"
+        {...props}
+    />
+);
+const Done = ({...props}) => (
+    <TouchableOpacity
+        style = {{marginHorizontal:8}}
+        {...props}>
+            <Text style = {{fontSiz:16}}>Done</Text>
+    </TouchableOpacity>
+);
 const OnboardingScreen = ({navigation}) => {
     return (
         <Onboarding
-            pages={[
+        SkipButtonComponent={Skip}
+        NextButtonComponent={Next}
+        DoneButtonComponent={Done}
+        onSkip={() => navigation.replace("Login")}
+        onDone={() => navigation.navigate("Login")}
+        pages={[
                 {
                     backgroundColor: '#fff',
                     image: <Image source={require('../assets/first.jpg')} />,
@@ -22,5 +48,4 @@ const OnboardingScreen = ({navigation}) => {
         />
     )
 }
-
 export default function OnboardingScreen;
