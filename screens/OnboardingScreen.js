9199f33f3517e9db9cec1820a4445e5155d5a92a
임성우 <seongwoorang@naver.com>
2021-03-19 20:37:07 +0900

    Update OnboardingScreen.js

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,51 +1,91 @@
-import React from 'react'
-import { View, Text, Button,Image, StyleSheet,TouchableOpacity } from 'react-native'
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
 import Onboarding from 'react-native-onboarding-swiper';
 
+const Dots = ({selected}) => {
+    let backgroundColor;
+
+    backgroundColor = selected ? 'rgba(0, 0, 0, 0.8)' : 'rgba(0, 0, 0, 0.3)';
+
+    return (
+        <View 
+            style={{
+                width:6,
+                height: 6,
+                marginHorizontal: 3,
+                backgroundColor
+            }}
+        />
+    );
+}
+
 const Skip = ({...props}) => (
-    <Button
-        title = 'Skip'
-        color="#000000"
-        {...props}
-    />
-);
-const Next = ({...props}) => (
-    <Button
-        title = 'Next'
-        color="#000000"
-        {...props}
-    />
-);
-const Done = ({...props}) => (
     <TouchableOpacity
-        style = {{marginHorizontal:8}}
-        {...props}>
-            <Text style = {{fontSiz:16}}>Done</Text>
+        style={{marginHorizontal:10}}
+        {...props}
+    >
+        <Text style={{fontSize:16}}>Skip</Text>
     </TouchableOpacity>
 );
+
+const Next = ({...props}) => (
+    <TouchableOpacity
+        style={{marginHorizontal:10}}
+        {...props}
+    >
+        <Text style={{fontSize:16}}>Next</Text>
+    </TouchableOpacity>
+);
+
+const Done = ({...props}) => (
+    <TouchableOpacity
+        style={{marginHorizontal:10}}
+        {...props}
+    >
+        <Text style={{fontSize:16}}>Done</Text>
+    </TouchableOpacity>
+);
+
 const OnboardingScreen = ({navigation}) => {
     return (
         <Onboarding
         SkipButtonComponent={Skip}
         NextButtonComponent={Next}
         DoneButtonComponent={Done}
+        DotComponent={Dots}
         onSkip={() => navigation.replace("Login")}
         onDone={() => navigation.navigate("Login")}
         pages={[
-                {
-                    backgroundColor: '#fff',
-                    image: <Image source={require('../assets/first.jpg')} />,
-                    title: 'Onboarding',
-                    subtitle: 'my second app',
-                },
-                {
-                    backgroundColor: '#fff',
-                    image: <Image source={require('../assets/wow.jpg')} />,
-                    title: 'Onboarding',
-                    subtitle: 'lets practice react native! ',
-                }
-             ]}
-        />
-    )
-}
-export default function OnboardingScreen;
+          {
+            backgroundColor: '#a6e4d0',
+            image: <Image source={require('../assets/onboarding-img1.png')} />,
+            title: 'Connect to the World',
+            subtitle: 'A New Way To Connect With The World',
+          },
+          {
+            backgroundColor: '#fdeb93',
+            image: <Image source={require('../assets/onboarding-img2.png')} />,
+            title: 'Share Your Favorites',
+            subtitle: 'Share Your Thoughts With Similar Kind of People',
+          },
+          {
+            backgroundColor: '#e9bcbe',
+            image: <Image source={require('../assets/onboarding-img3.png')} />,
+            title: 'Become The Star',
+            subtitle: "Let The Spot Light Capture You",
+          },
+        ]}
+      />
+    );
+};
+
+export default OnboardingScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+});
